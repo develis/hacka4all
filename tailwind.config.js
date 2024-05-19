e0ebbc7592adb5d521594eff88febfa0c56cfbbb
Monique Silva <moniquesntsilva@gmail.com>
2024-05-18 21:26:01 -0300

    feat: prettier e conf tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,vue}",
-  ],
+  content: ['./index.html', './src/**/*.{js,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'brand-white': 'hsl(0, 0%, 100%)',
+        'brand-black': 'hsl(0, 0%, 0%)',
+        'brand-dark-gray': 'hsl(0, 0%, 55%)',
+      },
+      backgroundColor: {
+        'brand-white': 'hsl(0, 0%, 100%)',
+        'brand-black': 'hsl(0, 0%, 0%)',
+        'brand-dark-gray': 'hsl(0, 0%, 55%)',
+      },
+      fontFamily: {
+        alata: ['Alata', 'sans-serif'],
+        'josefin-sans': ['Josefin Sans', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
