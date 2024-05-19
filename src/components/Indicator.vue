f51ee817379e55ddb0f5951683cc63bba10e202d
Monique Silva <moniquesntsilva@gmail.com>
2024-05-19 02:00:02 -0300

    prettier

--- src/components/Indicator.vue
+++ src/components/Indicator.vue
@@ -2,9 +2,12 @@
   <section class="py-24 w-full bg-brand-white">
     <div class="mx-auto max-w-[1280px]">
       <div class="bg-white rounded-lg shadow-md p-4 flex items-center">
-        <p class="font-bold text-3xl mr-4"> 01 </p>
+        <p class="font-bold text-3xl mr-4">01</p>
         <div class="flex-grow">
-          <p class="font-josefin-sans text-2xl mt-2"> Aumento da <br> lucratividade </p>
+          <p class="font-josefin-sans text-2xl mt-2">
+            Aumento da <br />
+            lucratividade
+          </p>
         </div>
       </div>
     </div>
@@ -12,10 +15,10 @@
 </template>
 
 <script>
-export default {
-  name: 'AppIndicator',
-  data() {
-    return {}
-  },
-};
+  export default {
+    name: 'AppIndicator',
+    data() {
+      return {}
+    },
+  }
 </script>
